3d8e8f2695a38785a82e53153ce0367c0a6efb03
Iris <iris@nanoleaf.me>
2022-01-18 17:14:24 +0800

    Update loading page

--- _dev/assets/js/loading.js
+++ _dev/assets/js/loading.js
@@ -1,26 +1,24 @@
 
-// const supportLoading = 'loading' in HTMLImageElement.prototype;
-// let targetSpan = document.getElementById('boolean');
-// targetSpan.innerHTML = supportLoading;
-// // console.log(supportLoading);
+    const supportLoading = 'loading' in HTMLImageElement.prototype;
+    // console.log(supportLoading);
+    let targetSpan = document.getElementById('boolean');
+    targetSpan.innerHTML = supportLoading;
 
-// if (supportLoading) {
-//   const imgTag = document.querySelectorAll('img[loading="lazy"]');
-//   imgTag.forEach(img => {
-//     let pictureTag = img.parentNode.children;
-//     // console.log(pictureTag);
-//     for (var i = 0; i < pictureTag.length; i++) {
-//       // console.log(pictureTag[i].dataset);
-//       if (pictureTag[i].dataset.srcset) {
-//         pictureTag[i].srcset = pictureTag[i].dataset.srcset
-//       }
-//       if (pictureTag[i].dataset.src) {
-//         pictureTag[i].src = pictureTag[i].dataset.src
-//       }
-//     }
-//   });
-// } else {
-//   const script = document.createElement('script');
-//   script.src = '/assets/js/lazysizes.min.js';
-//   document.body.appendChild(script);
-// }
+    if (supportLoading) {
+      const imgTag = document.querySelectorAll('img[loading="lazy"]');
+      imgTag.forEach(function(img) {
+        let pictureTag = img.parentNode.children;
+        for (var i = 0; i < pictureTag.length; i++) {
+          if (pictureTag[i].dataset.srcset) {
+            pictureTag[i].srcset = pictureTag[i].dataset.srcset
+          }
+          if (pictureTag[i].dataset.src) {
+            pictureTag[i].src = pictureTag[i].dataset.src
+          }
+        }
+      })
+    } else {
+      const script = document.createElement('script');
+      script.src = '/assets/js/lazysizes.min.js';
+      document.body.appendChild(script);
+    }
